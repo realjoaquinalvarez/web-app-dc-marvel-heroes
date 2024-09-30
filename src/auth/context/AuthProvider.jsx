6e44c7b7e851ab83d9fac1b020b028cfb8dfb552
Joaquin <clcflecha@gmail.com>
2024-09-30 10:27:32 -0400

    Auth Provider

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,32 @@
+import { useReducer } from "react"
+import { AuthContext } from "./AuthContext"
+import { authReducer } from "./authReducer"
+import { types } from "../../types/types"
+
+const initialState = {
+    logged: false,
+}
+
+
+export const AuthProvider = ({ children }) => {
+
+    const [ authState, dispatch ] = useReducer( authReducer, initialState)
+
+    const login = ( name = '' ) => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: 'ABC',
+                name: name,
+            }
+        }
+        
+        dispatch(action)
+    }
+
+    return(
+        <AuthContext.Provider value={{ }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
